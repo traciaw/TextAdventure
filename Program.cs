e31b5d637222b6506c62f1573a5f23b4151cfcb8
Tracia Wee <traciawee@gmail.com>
2022-10-15 16:52:12 +0800

    more theatrics
    dealys and color changes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace ChooseYourOwnAdventure
 {
@@ -27,18 +28,28 @@ namespace ChooseYourOwnAdventure
             // Start by asking for the user's name:
             Console.Write("What is your name?: ");
             string name = Console.ReadLine();
+            Thread.Sleep(1000);
             Console.WriteLine($"{name}~~ {name}~~ BOOM!");
-
-            Console.WriteLine("Thunder boomed in the distance.You woke up in a unfamilar cave. It looked like it will rain soon.");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine($"{name}~~ {name}~~ BOOM!");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine($"{name}~~ {name}~~ BOOM!");
+            Console.Clear();
+            Console.WriteLine($"{name}~~ {name}~~ BOOM!");
+            Thread.Sleep(1000);
+            Console.WriteLine("Thunder boomed in the distance.You woke up in an unfamilar cave. Looks like it will rain soon.");
             Console.WriteLine("Do you choose to stay in the cave?");
             Console.Write("Type YES or NO: ");
             string StayCave = Console.ReadLine().ToUpper();
-            if (StayCave == "NO")
+            if (StayCave == "NO" || StayCave == "N")
             {
                 Console.Clear();
                 Console.WriteLine("You choose to leave the cave.");
                 Console.WriteLine("Whoever brought me here might return soon. We should leave now!");
-                Console.WriteLine("You creeped out of the cave. Straight ahead you see a town in the distance. On your left, you see a crooked-looking cottage.");
+                Thread.Sleep(1000);
+                Console.WriteLine("You creeped out of the cave. Straight ahead you see a town in the distance.");
+                Console.WriteLine("On your left, you see a crooked-looking cottage.");
                 Console.Write("Type 1 to walk towards the TOWN or 2 to go towards the COTTAGE: ");
                 string PathChoice = Console.ReadLine().ToUpper();
                 switch(PathChoice)
@@ -49,14 +60,18 @@ namespace ChooseYourOwnAdventure
                         Town();
                         Console.WriteLine(" ");
                         Console.WriteLine("'BOOM!' Thunder roared overhead and dark clouds spat out beads of water.");
+                        Thread.Sleep(1000);
                         Console.WriteLine("Ahead you spotted a figure holding an umbrella beckoning you over.");
+                        Thread.Sleep(1000);
                         Console.Write("Type 1 to APPROACH the figure or 2 to IGNORE and continue walking: ");
                         string FigureChoice = Console.ReadLine().ToUpper();
                         if(FigureChoice=="1")
                         {
                             Console.Clear();
                             Console.WriteLine("You approach the figure...");
+                            Thread.Sleep(1000);
                             Console.WriteLine("As you get closer, you saw that the figure was an eyeless abomination with seven mouths...");
+                            Thread.Sleep(2000);
                             Slip();
                             Wake();
                         }
@@ -64,6 +79,7 @@ namespace ChooseYourOwnAdventure
                         {
                             Console.Clear();
                             Console.WriteLine("One should avoid strangers....");
+                            Thread.Sleep(2000);
                             Slip();
                             Wake();
                         }
@@ -73,21 +89,28 @@ namespace ChooseYourOwnAdventure
                     case "2":
                         Console.Clear();
                         Console.WriteLine("You set out for cottage");
+                        Thread.Sleep(1000);
                         Cottage();
                         Console.WriteLine("It looks abandoned...");
+                        Thread.Sleep(2000);
                         Console.WriteLine("Should you walk through the front entrance or sneak around the back?");
+                        Thread.Sleep(1000);
                         Console.Write("Type 1 to WALK through the entrance or or 2 to SNEAK around: ");
                         string WalkSneakChoice = Console.ReadLine().ToUpper();
                         if(WalkSneakChoice=="1")
                         {
                             Console.WriteLine("You walked into the dark cottage");
+                            Thread.Sleep(1000);
                             Console.WriteLine("The wooden flooring creaks with every step...");
+                            Thread.Sleep(1000);
                             WhisperRiddle();
                         }
                         else if(WalkSneakChoice=="2")
                         {
                             Console.Clear();
+                            Thread.Sleep(1000);
                             Console.WriteLine("You choose to sneak around instead");
+                            Thread.Sleep(2000);
                             Slip();
                             Wake();
                         }
@@ -98,25 +121,31 @@ namespace ChooseYourOwnAdventure
                     break;
                 }
             }
-            else if (StayCave == "YES")
+            else if (StayCave == "YES"|| StayCave == "Y")
             {
                 Console.Clear();
                 Console.WriteLine("You choose to stay within the cave.");
+                Thread.Sleep(2000);
                 Console.WriteLine("The thunder is deafening outside. Staying sheltered is obviously the better choice.");
+                Thread.Sleep(1000);
                 Console.WriteLine("You spotted a narrow passageway within the cave. Should you explore?");
+                Thread.Sleep(1000);
                 Console.Write("Type 1 to EXPLORE or 2 to STAY where you are:");
                 string PassagewayChoice = Console.ReadLine().ToUpper();
                 if (PassagewayChoice == "1")
                 {
                     Console.Clear();
                     Console.WriteLine("You squeezed into the damp passageway...");
+                    Thread.Sleep(1000);
                     WhisperRiddle();
                 }//PassagewayChoice == "1" END
                 else if (PassagewayChoice == "2")
                 {
                     Console.Clear();
                     Console.WriteLine("You look around and see an off drawing on the far side of the cave.");
+                    Thread.Sleep(2000);
                     Console.WriteLine("Weird...The face looks like a... Monster/Ghost/Bug/Skeleton?");
+                    Thread.Sleep(1000);
                     Console.WriteLine("Enter a number (1-4): ");
                     Console.WriteLine("1: Monster");
                     Console.WriteLine("2: Ghost");
@@ -127,38 +156,53 @@ namespace ChooseYourOwnAdventure
                     switch (HeadChoice)
                     {
                         case "1":
+                            Thread.Sleep(3000);
                             MonsterHead();
-                            Console.WriteLine("  ~~~~~~~~~ ");
+                            Console.WriteLine(" ");
+                            Thread.Sleep(2000);
                             Console.WriteLine("'GRRRRRAAAA!!!' You turn to see a dark shadowy figure behind you. You should have left..");
                             Console.WriteLine(" ");
+                            Thread.Sleep(2000);
                             BuildACreature("Monster","Monster","Monster");
                             Console.WriteLine("    THE END");
                             ReStart();
                             
                             break;
                         case "2":
+                            Thread.Sleep(3000);
                             GhostHead();
-                            Console.WriteLine("  ~~~~~~~~~ ");
-                            Console.WriteLine("'GRRRRRAAAA!!!' You turn to see a dark shadowy figure behind you. You should have left..");
+                            Console.WriteLine("  ");
+                            Thread.Sleep(2000);
+                            Console.WriteLine("'GRRRRRAAAA!!!' You turn to see a dark shadowy figure behind you.");
+                            Console.WriteLine("You should have left..");
                             Console.WriteLine(" ");
+                            Thread.Sleep(2000);
                             BuildACreature("Ghost","Ghost","Ghost");
                             Console.WriteLine("    THE END");
                             ReStart();
                             break;
                         case "3":
+                            Thread.Sleep(3000);
                             BugHead();
-                            Console.WriteLine("  ~~~~~~~~~ ");
-                            Console.WriteLine("'GRRRRRAAAA!!!' You turn to see a dark shadowy figure behind you. You should have left..");
                             Console.WriteLine(" ");
+                            Thread.Sleep(2000);
+                            Console.WriteLine("'GRRRRRAAAA!!!' You turn to see a dark shadowy figure behind you. You should have left..");
+                            Console.WriteLine("You should have left..");
+                            Console.WriteLine(" ");
+                            Thread.Sleep(2000);
                             BuildACreature("Bug","Bug","Bug");
                             Console.WriteLine("    THE END");
                             ReStart();
                             break;
                         case "4":
+                            Thread.Sleep(3000);
                             SkeletonHead();
-                            Console.WriteLine("  ~~~~~~~~~ ");
+                            Console.WriteLine("  ");
+                            Thread.Sleep(2000);
                             Console.WriteLine("'GRRRRRAAAA!!!' You turn to see a dark shadowy figure behind you. You should have left..");
+                            Console.WriteLine("You should have left..");
                             Console.WriteLine(" ");
+                            Thread.Sleep(2000);
                             BuildACreature("Skeleton","Skeleton","Skeleton");
                             Console.WriteLine("     THE END");
                             ReStart();
@@ -169,6 +213,7 @@ namespace ChooseYourOwnAdventure
                     };//HeadChoice end
 
                 }//PassagewayChoice == "2" END
+                else { ShitTyping(); }
             }//StayCave == "YES" END
             else
             { ShitTyping();}//StayCave == "Invalid" END
@@ -177,25 +222,38 @@ namespace ChooseYourOwnAdventure
         //STORY Shortcuts
         static void ShitTyping()//When user cant type right. END GAME
         {
-            Console.WriteLine("");
-            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
             Console.WriteLine("Your vision blurs... You obviously can't think right. You feel an ominous aura behind you...");
+            Thread.Sleep(2000);
             Console.WriteLine("The last thing you see was a monster before the darkness swallows you.");
+            Thread.Sleep(2000);
             RandomMonsterMode();
             Console.WriteLine("      THE END");
             ReStart();
         }
         static void WhisperRiddle()
         {
-            Console.WriteLine("A voice behind you whispers. It says, 'Answer this riddle: What is so fragile that saying its name breaks it?'");
+            Console.WriteLine("A voice behind you whispers...");
+            Thread.Sleep(1000);
+            Console.WriteLine("It says, 'Answer this riddle: What is so fragile that saying its name breaks it?'");
             Console.Write("Type your answer: ");
             string riddleAnswer = Console.ReadLine().ToUpper();
             if (riddleAnswer.Contains("SILENCE")||riddleAnswer.Contains("SILENT")||riddleAnswer.Contains("QUIET"))
             {
                 Console.Clear();
                 Console.WriteLine("The same voice replies:'You answered correctly.You are safe mortal. For now...'");
+                Thread.Sleep(2000);
                 Console.WriteLine("You turn around and see a mouse scurrying away into the cracks...'");
+                Thread.Sleep(1000);
                 Mouse();
+                Thread.Sleep(1000);
                 Console.WriteLine("Turning back, you see a bottle with the label 'DRINK ME!' and a box of cookies with the label 'EAT ME!'");
                 Console.WriteLine("Do you reach out for the BOTTLE or COOKIES first?");
                 Console.Write("Type your answer: ");
@@ -204,17 +262,27 @@ namespace ChooseYourOwnAdventure
                 {
                     Console.Clear();
                     Console.WriteLine("You choose to drink from tiny bottle.");
+                    Console.WriteLine("");
+                    Thread.Sleep(1000);
                     Drink();
-                    Console.WriteLine("The liquid that hit your tongue was thick and slimy. EWWW....");
+                    Console.WriteLine($"The liquid that hit your tongue was thick and slimy. ");
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"                                                      EWWW....");
+                    Thread.Sleep(2000);
                     Console.WriteLine("The head starts to spin.. Oh no...");
                     Wake();
                 }
                 else if(FoodChoice.Contains("COOKIE")||FoodChoice.Contains("EAT")||FoodChoice.Contains("BOX"))
                 {
                     Console.Clear();
-                    Console.WriteLine("Choosing to eat from the box of cookies, you found it dry and chalky. YUCK!");
                     Cookie();
+                    Console.WriteLine("Choosing to eat from the box of cookies, you found it dry and chalky.");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("                                                                      YUCK!");
+                    Console.WriteLine("");
+                    Thread.Sleep(2000);
                     Console.WriteLine("The head starts to spin.. Oh no...");
+                    Thread.Sleep(2000);
                     Wake();
                 }
                 else
@@ -222,7 +290,16 @@ namespace ChooseYourOwnAdventure
             }
             else
             {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Clear();
                 Console.WriteLine("'Incorrect', the same voice whispers. The last thing you see was a monster before the darkness took you.");
+                Thread.Sleep(1000);
                 RandomMonsterMode();
                 Console.WriteLine("     THE END");
                 ReStart();
@@ -231,8 +308,17 @@ namespace ChooseYourOwnAdventure
         static void Slip()
         {
             Console.WriteLine("'CLICKED!'");
+            Thread.Sleep(2000);
             Console.WriteLine("You didn't see the bear trap on the ground as it clamps into your leg...");
+            Thread.Sleep(1000);
             Console.WriteLine("AHHHHH!!");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Clear();
+            Console.WriteLine("AHHHHH!!");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("AHHHHH!!");
+            Thread.Sleep(5000);
         }
         static void Wake()
         {
@@ -240,10 +326,15 @@ namespace ChooseYourOwnAdventure
             Console.WriteLine("");
             Console.WriteLine("You woke with a start in your bedroom.");
             Console.WriteLine("That was a weird dream...");
+            Thread.Sleep(2000);
             Console.WriteLine("In the corner of your eye, a dark shadowy figure creeps");
+            Thread.Sleep(1000);
             Console.WriteLine("                                                         pass");
+            Thread.Sleep(1000);
             Console.WriteLine("                                                             the");
+            Thread.Sleep(1000);
             Console.WriteLine("                                                                 door...");
+            Thread.Sleep(2000);
             RandomMonsterMode();
             Console.WriteLine("      THE END");
             ReStart();
